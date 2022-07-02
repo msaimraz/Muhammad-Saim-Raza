65db5f367d2aa87f788f5d66bb5daced85f28723
Muhammad Saim Raza <msraza.02@gmail.com>
2022-07-02 10:58:08 +0500

    mobile img

--- src/views/Preview.vue
+++ src/views/Preview.vue
@@ -9,7 +9,28 @@
 
     <span class="intro_line"></span><span class="intro_line_2"></span><span class="intro_line_3"></span>
 
-    <div class="demos">
+    <div class="demos demoTab">
+      <div class="left">
+        <router-link to="/home-light">
+          <div class="desc">
+            <img src="../assets/img/intro/light2.png" alt="light version demo" />
+            <h3 class="title">Light Mode</h3>
+          </div>
+        </router-link>
+      </div>
+      <!-- End .left -->
+      <div class="right">
+        <router-link to="/home-dark">
+          <div class="desc">
+            <img src="../assets/img/intro/dark2.png" alt="dark version demo" />
+            <h3 class="title">Dark Mode</h3>
+          </div>
+        </router-link>
+      </div>
+      <!-- End .right -->
+    </div>
+
+    <div class="demos demoLap">
       <div class="left">
         <router-link to="/home-light">
           <div class="desc">
@@ -32,6 +53,7 @@
     <!-- End .demos -->
   </div>
   <!-- End .tokyo_tm_intro -->
+  
 </template>
 
 <script>
